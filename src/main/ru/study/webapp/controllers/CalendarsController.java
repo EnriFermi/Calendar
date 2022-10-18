4e383d9ad0afe17c118f48a4309682f5ee71210f
Романихин Валерий Александрович <romanihin@unislabs.com>
2022-10-18 18:16:08 +0300

    предложения по рефаторингу

--- src/main/ru/study/webapp/controllers/CalendarsController.java
+++ src/main/ru/study/webapp/controllers/CalendarsController.java
@@ -22,64 +22,78 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @ComponentScan("ru.study.webapp")
 @RequestMapping("/calendar")
 public class CalendarsController {
-    private final DatabaseMapper mapper = Mappers.getMapper(DatabaseMapper.class);
-    private final CalendarRepository repository;
-    private final CalendarModelAssembler assembler;
-    CalendarsController(CalendarRepository repository, CalendarModelAssembler assembler) {
-        this.repository = repository;
-        this.assembler = assembler;
-    }
-    @GetMapping("/")
-    public CollectionModel<EntityModel<CalendarDAO>> all() {
-        List<EntityModel<CalendarDAO>> calendars = StreamSupport.stream(repository.findAll().spliterator(), false)
-                .map(calendarDAO -> assembler.toModel(calendarDAO))
-                .collect(Collectors.toList());
+  private final DatabaseMapper mapper = Mappers.getMapper(DatabaseMapper.class);
+  private final CalendarRepository repository;
+  private final CalendarModelAssembler assembler;
 
-        return CollectionModel.of(calendars, linkTo(methodOn(CalendarsController.class).all()).withSelfRel());
+  CalendarsController(CalendarRepository repository, CalendarModelAssembler assembler) {
+    this.repository = repository;
+    this.assembler = assembler;
+  }
+
+  @GetMapping("/")
+  public CollectionModel<EntityModel<CalendarDAO>> all() {
+    List<EntityModel<CalendarDAO>> calendars = StreamSupport.stream(repository.findAll().spliterator(), false)
+        .map(calendarDAO -> assembler.toModel(calendarDAO))
+        .collect(Collectors.toList());
+
+    return CollectionModel.of(calendars, linkTo(methodOn(CalendarsController.class).all()).withSelfRel());
+  }
+
+  @GetMapping("/{id}")
+  public EntityModel<CalendarDAO> one(@PathVariable Long id) {
+    return assembler.toModel(repository.findById(id)
+        .orElseThrow(() -> new CalendarNotFoundException(id)));
+  }
+
+  @PostMapping("/")
+  public EntityModel<CalendarDAO> newEmployee(@RequestBody CalendarDAO calendarDAO) {
+    for (int i = 0; i < calendarDAO.getDayList().size(); i++) {
+      if (calendarDAO.getDayList().get(i).equals(calendarDAO.getAnchorWeekDay())) {
+        calendarDAO.setAnchorWeekDay(calendarDAO.getDayList().get(i));
+        break;
+      }
+    }
+    return assembler.toModel(repository.save(calendarDAO));
+  }
+
+  @PutMapping("/{id}")
+  //TODO архитектура: контроллер -> сервис(если надо открывает тразакцию) -> репозиториями
+  //контроллер (ДТО - json + schema) -> сервис( доменная модель, чисто класс с getter+setter) -> репозиториями (entity -> БД)
+  public EntityModel<CalendarDAO> replaceCalendar(@RequestBody CalendarDAO calendarDAO, @PathVariable Long id) {
+
+    //TODO можно разбить на кучу контроллеров, например меняющий с какого дня недели стартует 0 год
+    for (int i = 0; i < calendarDAO.getDayList().size(); i++) {
+      if (calendarDAO.getDayList().get(i).equals(calendarDAO.getAnchorWeekDay())) {
+        calendarDAO.setAnchorWeekDay(calendarDAO.getDayList().get(i));
+        break;
+      }
     }
 
-    @GetMapping("/{id}")
-    public EntityModel<CalendarDAO> one(@PathVariable Long id) {
-        return assembler.toModel(repository.findById(id)
-                .orElseThrow(() -> new CalendarNotFoundException(id)));
-    }
+    //TODO если решил делать все на все, тогда надо calendarDAO = repository.findById(id), и в него прокидывать простые объекты при различии
+//       а если зависимый объект отличается, то надо конкретный заменять
 
-    @PostMapping("/")
-    public EntityModel<CalendarDAO> newEmployee(@RequestBody CalendarDAO calendarDAO) {
-        for(int i=0; i<calendarDAO.getDayList().size(); i++) {
-            if(calendarDAO.getDayList().get(i).equals(calendarDAO.getAnchorWeekDay())) {
-                calendarDAO.setAnchorWeekDay(calendarDAO.getDayList().get(i));
-                break;
-            }
-        }
-        return assembler.toModel(repository.save(calendarDAO));
-    }
 
-    @PutMapping("/{id}")
-    public EntityModel<CalendarDAO> replaceCalendar(@RequestBody CalendarDAO calendarDAO, @PathVariable Long id) {
-        for(int i=0; i<calendarDAO.getDayList().size(); i++) {
-            if(calendarDAO.getDayList().get(i).equals(calendarDAO.getAnchorWeekDay())) {
-                calendarDAO.setAnchorWeekDay(calendarDAO.getDayList().get(i));
-                break;
-            }
-        }
-        return repository.findById(id)
-                .map(calendar -> {
-                    mapper.updateCalendarDAO(calendarDAO, calendar);
-                    return assembler.toModel(repository.save(calendar));
-                })
-                .orElseGet(() -> {
-                    calendarDAO.setId(id);
-                    return assembler.toModel(repository.save(calendarDAO));
-                });
+    //контроллер (ДТО - json + schema) -> сервис( доменная модель, чисто класс с getter+setter) -> репозиториями (entity -> БД)/
+    // при парсинге в доменную проверяешь допустимость изменений. А ее заливаешь в существующую энтити.
+    return repository.findById(id)
+        .map(calendar -> {
+          mapper.updateCalendarDAO(calendarDAO, calendar);
+          return assembler.toModel(repository.save(calendar));
+        })
+        .orElseGet(() -> {
+          calendarDAO.setId(id);
+          return assembler.toModel(repository.save(calendarDAO));
+        });
+  }
+
+  @DeleteMapping("/{id}")
+  EntityModel<CalendarDAO> deleteCalendar(@PathVariable Long id) {
+    if (!repository.existsById(id)) {
+      throw new CalendarNotFoundException(id);
+    } else {
+      repository.deleteById(id);
+      return EntityModel.of(new CalendarDAO(), linkTo(methodOn(CalendarsController.class).all()).withRel("calendar"));
     }
-    @DeleteMapping("/{id}")
-    EntityModel<CalendarDAO> deleteCalendar(@PathVariable Long id) {
-        if(!repository.existsById(id)) {
-            throw new CalendarNotFoundException(id);
-        } else {
-            repository.deleteById(id);
-            return EntityModel.of(new CalendarDAO(), linkTo(methodOn(CalendarsController.class).all()).withRel("calendar"));
-        }
-    }
+  }
 }
